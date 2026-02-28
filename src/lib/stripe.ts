1a0a683570a5766ee4aae86814678e1f780b13d8
aniegboks <aniegbokavictor2024@gmail.com>
2026-02-28 22:24:26 +0100

    fix bugs

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -5,7 +5,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2025-04-30.basil",
+  apiVersion: "2025-08-27.basil",
 });
 
 export default stripe;
